be34edf217902ef796bed997c2737becfb75b3f4
Akansha <av0753484@gmail.com>
2023-02-07 13:35:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,11 +40,8 @@ if image_file is not None:
     
 # Classify the image
     output = classify_image(model, image)
-    class_labels = {0:"1", 1:"2",2: "3",3: "4",4: "5",5: "6",6: "7",7: "8",8: "9",
-                    9:"A", 10: "B", 11: "C", 12: "D", 13: "E", 14: "F", 15: "G", 16: "H", 17: "I", 18:"J",
-                    19: "K", 20: "L", 21: "M", 22: "N", 23: "O", 24: "P", 25: "Q", 26: "R", 27: "S", 28: "T",
-                    29:"U", 30: "V", 31: "W", 32: "X", 33: "Y", 34: "Z"}
-    class_index = (np.argmax(output))
-    class_label = class_labels[class_index]
-    st.write(class_label)
+    label = "Class: " + str(np.argmax(output))
+    if label == 'Class 12':
+        print("C")
+    st.write(label)
     
