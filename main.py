3e155e0ac86deaf8abb8dadff20bb358c5905e32
Akansha <av0753484@gmail.com>
2023-02-07 13:11:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ if image_file is not None:
     class_labels = {1:"1", 2:"2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10:"A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I",
                     19:"J", 20: "K", 21: "L", 22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R", 28: "S", 29: "T", 30:"U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z"}
     class_index = np.argmax(output)
-    output = classify_image(model, image)
+    class_label = class_labels[class_index]
+    st.text(class_label)
     num_classes = output.shape[1]
 
     all_classes = []
@@ -52,8 +53,6 @@ if image_file is not None:
         class_label = class_labels[i]
         all_classes.append((class_label, class_prob))
 
-st.write("Class probabilities:", all_classes)
+        st.write("Class probabilities:", all_classes)
 # Get the corresponding class label using the class_labels dictionary
-    class_label = class_labels[class_index]
-    st.text(class_label)
-    print("h")
+    
