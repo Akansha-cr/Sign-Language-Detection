7df9445dfb8572fea3a1880f86b5681fdd125c59
Akansha <av0753484@gmail.com>
2023-02-07 13:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,10 @@ if image_file is not None:
     
 # Classify the image
     output = classify_image(model, image)
-    class_labels = {1:"1", 2:"2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10:"A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I",
-                    19:"J", 20: "K", 21: "L", 22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R", 28: "S", 29: "T", 30:"U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z"}
+     class_labels = {'Class 1':"1", 'Class 2':"2",'Class 3': "3",'Class 4': "4",'Class 5': "5",'Class 6': "6",'Class 7': "7",'Class 8': "8",'Class 9': "9",
+                    'Class 10':"A",'Class 11': "B",'Class 12': "C",'Class 13': "D",'Class 14': "E",'Class 15': "F",'Class 16': "G",'Class 17': "H",'Class 18': "I",'Class 19':"J",
+                    'Class 20': "K",'Class 21': "L",'Class 22': "M",'Class 23': "N",'Class 24': "O",'Class 25': "P",'Class 26': "Q",'Class 27': "R",'Class 28': "S",'Class 29': "T",
+                    'Class 30':"U",'Class 31': "V",'Class 32': "W",'Class 33': "X",'Class 34': "Y",'Class 35': "Z"}
     class_index = (np.argmax(output))
     label = "Class: " + str(np.argmax(output))
     class_label = class_labels[label]
